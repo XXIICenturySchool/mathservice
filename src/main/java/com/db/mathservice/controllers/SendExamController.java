a6bc23aa91caf83f07ee2f554134213d4927b114
Andrey Golikov <golikov.andrei@gmail.com>
2017-09-06 16:28:45 +0300

    frontend working

--- src/main/java/com/db/mathservice/controllers/SendExamController.java
+++ src/main/java/com/db/mathservice/controllers/SendExamController.java
@@ -1,21 +1,24 @@
 package com.db.mathservice.controllers;
 
 import com.db.mathservice.data.ExerciseConfigurationCounted;
+import com.db.mathservice.data.InputExamConfiguration;
 import com.db.mathservice.data.InputExerciseConfiguration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * Created by Valentin on 06.09.2017.
  */
 @RestController
-public class OloloController {
+public class SendExamController {
 
-    @PostMapping("/ololo")
-    public String printOlolo(@RequestBody InputExerciseConfiguration inputConfiguration) {
+    @PostMapping("/send_exam")
+    public String addExam(@RequestBody InputExamConfiguration inputConfiguration) {
         System.out.println("inputConfiguration = " + inputConfiguration);
-        return "ololo";
+        return "exams";
     }
 
 }
